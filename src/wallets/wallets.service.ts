4049768917a58eb4d362c17baac7af259230569c
Fagner Dev <fagnersro.dev@gmail.com>
2025-02-20 22:50:24 -0300

    registration and search of wallets-assets

--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -3,10 +3,15 @@ import { CreateWalletDto } from './dto/create-wallet.dto';
 import { Wallet } from './entities/wallet.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { WalletAsset } from './entities/wallet-asset.entity';
 
 @Injectable()
 export class WalletsService {
-  constructor(@InjectModel(Wallet.name) private WalletSchema: Model<Wallet>) {}
+  constructor(
+    @InjectModel(Wallet.name) private WalletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name)
+    private walletAssetSchema: Model<WalletAsset>,
+  ) {}
 
   create(CreateWalletDto: CreateWalletDto) {
     return this.WalletSchema.create(CreateWalletDto);
@@ -18,5 +23,17 @@ export class WalletsService {
 
   findOne(id: string) {
     return this.WalletSchema.findById(id);
+  }
+
+  createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number;
+  }) {
+    return this.walletAssetSchema.create({
+      wallet: data.walletId,
+      asset: data.assetId,
+      shares: data.shares,
+    });
   }
 }
